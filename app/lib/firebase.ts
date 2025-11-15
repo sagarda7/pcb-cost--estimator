e4b5662506e611810ed83d958fd5228d576eccf7
Sagar Devkota <sagarda7@gmail.com>
2025-11-15 12:33:11 +0545

    firebase credentials to .env

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-apiKey: "AIzaSyBSI-eoIdtSOo-oA3-N7zy3yqbGSw0m1H4",
-authDomain: "techasdy-846d7.firebaseapp.com",
-projectId: "techasdy-846d7",
-storageBucket: "techasdy-846d7.firebasestorage.app",
-messagingSenderId: "220151326032",
-appId: "1:220151326032:web:5fd4bcbbff00267c59f855",
-measurementId: "G-7RF3THQJ4F"
+    apiKey: "AIzaSyBSI-eoIdtSOo-oA3-N7zy3yqbGSw0m1H4",
+    authDomain: "techasdy-846d7.firebaseapp.com",
+    projectId: "techasdy-846d7",
+    storageBucket: "techasdy-846d7.firebasestorage.app",
+    messagingSenderId: "220151326032",
+    appId: "1:220151326032:web:5fd4bcbbff00267c59f855",
+    measurementId: "G-7RF3THQJ4F"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
